bdd9f3377f461307593028e1d01fdce638eeb8c3
Robin <robin.bijdeleij@gmail.com>
2017-02-11 23:23:58 +0100

    [Removed] Bootstrapper

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/src/Bootstrap.php';
+require_once __DIR__ . '/src/Main.php';
 
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode((new \GeoIp\Main)->Run());
