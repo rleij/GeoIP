904a438d83da49f67a3184e7f6f218c9365d3d72
Robin <robin.bijdeleij@gmail.com>
2017-02-12 14:55:52 +0100

    [Improved] Cleaner json response

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 
 require_once __DIR__ . '/src/Main.php';
 
-header("Content-Type: application/json; charset=UTF-8");
-
 $main = new \GeoIp\Main;
 $result = $main->Run();
 
-echo json_encode($result);
+header("Content-Type: application/json; charset=UTF-8");
+echo json_encode([
+    'ip_address' => $result->traits->ipAddress,
+    'timezone' => $result->location->timeZone,
+    'continent' => [
+        'geoname_id' => $result->continent->geonameId,
+        'iso_code' => $result->continent->code,
+        'name' => $result->continent->name
+    ],
+    'country' => [
+        'geoname_id' => $result->country->geonameId,
+        'iso_code' => $result->country->isoCode,
+        'name' => $result->country->name
+    ],
+    'city' => [
+        'geoname_id' => $result->city->geonameId,
+        'name' => $result->city->name
+    ],
+    'location' => [
+        'accuracy' => $result->location->accuracyRadius,
+        'latitude' => $result->location->latitude,
+        'longitude' => $result->location->longitude
+    ]
+]);
 
 ?>
